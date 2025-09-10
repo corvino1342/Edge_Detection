33067896a6c5d8e8e2957efd858dc046396a2ff3
corvino <corvino1342@gmail.com>
2025-09-10 10:47:45 +0200

    LoG and DoG methods added

--- main.py
+++ main.py
@@ -22,17 +22,25 @@ sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
 # Canny Edge Detection
 edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
 
+log = cv2.Laplacian(img_blur, ddepth=cv2.CV_64F)
+
+sigma1 = 1
+sigma2 = 2
+blur1 = cv2.GaussianBlur(img_gray, (0, 0), sigmaX=sigma1)
+blur2 = cv2.GaussianBlur(img_gray, (0, 0), sigmaX=sigma2)
+dog = blur1.astype('float32') - blur2.astype('float32')
+
 # display all the images
-titles = ['Original', 'Gray', 'Blurred', 'Sobel X', 'Sobel Y', 'Sobel XY', 'Canny']
-images = [img, img_gray, img_blur, sobelx, sobely, sobelxy, edges]
+titles = ['Original', 'Gray', 'Blurred', 'Sobel X', 'Sobel Y', 'Sobel XY', 'Canny', 'LoG', 'DoG']
+images = [img, img_gray, img_blur, sobelx, sobely, sobelxy, edges, log, dog]
 
 plt.figure(figsize=(15, 5))
 for i in range(len(images)):
-    plt.subplot(1, len(images), i+1)
+    #plt.subplot(3, int(len(images)/2+0.1), i+1)
     if len(images[i].shape) == 2:
         plt.imshow(images[i], cmap='gray')
     else:
         plt.imshow(cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB))
     plt.title(titles[i])
     plt.axis('off')
-plt.show()
+    plt.show()
