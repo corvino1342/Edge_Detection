76556bf4e1bf0ebb9de5f895a0e7dcf3f2d0985a
corvino <corvino1342@gmail.com>
2025-09-10 12:59:07 +0200

    Adding the kernels and the absolute values to get the correct (I hope) images highlighting the edges

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-
-def q_gaussian_kernel(ksize = 5, beta = 1.0, q = 1.0):
+def q_gaussian_kernel(ksize = 5, sigma = 1.0, q = 1.0):
     """
     Generate a 2D q-Gaussian kernel.
 
     Args:
         ksize (int): kernel size (odd number, e.g. 3, 5, 7).
-        beta (float): scale parameter (like 1/(2*sigma^2) for Gaussian).
+        sigma (float): scale parameter (the sigma for Gaussian).
         q (float): entropic index (q=1 → normal Gaussian).
 
     Returns:
@@ -24,6 +24,8 @@ def q_gaussian_kernel(ksize = 5, beta = 1.0, q = 1.0):
     X, Y = np.meshgrid(x, y)
     R2 = X**2 + Y**2
 
+    beta = (1 / (2 * sigma ** 2)) # this is the scale parameter, basaed on the value of Gaussian sigma
+
     if q == 0:
         kernel = np.exp(-beta * R2)
     else:
@@ -32,6 +34,8 @@ def q_gaussian_kernel(ksize = 5, beta = 1.0, q = 1.0):
 
     kernel /= kernel.sum()
     return kernel.astype(np.float32)
+
+
 
 # importing the image
 img = cv2.imread("mouse_picture.png")
@@ -44,41 +48,74 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #  blurring the image
 img_blur = cv2.GaussianBlur(img_gray, (3, 3), sigmaX=0, sigmaY=0)
-cv2.imshow("Blurred", img_blur)
+
+# blurring the image using q-gaussian kernel
+kernel_q = q_gaussian_kernel(ksize=7, sigma=0.5, q=1.2)
+img_qblur = cv2.filter2D(img_gray, -1, kernel_q)
 
 # Sobel Edge Detection
-sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
-sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
-sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+print('Sobel')
+start = time.time()
+sobel = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=3)
+sobel = np.abs(sobel)   # convert to magnitude
+print(f'Time spent: {(time.time() - start):.6f} seconds\n')
 
 # Canny Edge Detection
-edges = cv2.Canny(image=img_blur, threshold1=20, threshold2=100)
+print('Canny')
+start = time.time()
+canny = cv2.Canny(image=img_blur, threshold1=150, threshold2=300)
+print(f'Time spent: {(time.time() - start):.6f} seconds\n')
 
-log = cv2.Laplacian(img_blur, ddepth=cv2.CV_64F)
+print('LoG')
+start = time.time()
+log_kernel = np.array([[0,  0, -1,  0,  0],
+                       [0, -1, -2, -1,  0],
+                       [-1,-2, 16, -2, -1],
+                       [0, -1, -2, -1,  0],
+                       [0,  0, -1,  0,  0]], dtype=np.float32)
+log = cv2.filter2D(img_gray, -1, log_kernel)
+log_norm = cv2.normalize(log, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+print(f'Time spent: {(time.time() - start):.6f} seconds\n')
 
 sigma1 = 1
 sigma2 = 2
+
+print('DoG')
+start = time.time()
 blur1 = cv2.GaussianBlur(img_gray, (0, 0), sigmaX=sigma1)
 blur2 = cv2.GaussianBlur(img_gray, (0, 0), sigmaX=sigma2)
 dog = blur1.astype('float32') - blur2.astype('float32')
 
-kernel_q = q_gaussian_kernel(ksize=7, beta=0.05, q=1.2)
+edge_kernel = np.array([[0,  1, 0],
+                        [1, -4, 1],
+                        [0,  1, 0]], dtype=np.float32)
+dog_edges = cv2.filter2D(dog, -1, edge_kernel)
+dog_edges = np.abs(dog_edges)   # keep only magnitude
+dog_norm = cv2.normalize(dog_edges, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
-img_qblur = cv2.filter2D(img_gray, -1, kernel_q)
+print(f'Time spent: {(time.time() - start):.6f} seconds\n')
 
-k1 = q_gaussian_kernel(ksize=3, beta=0.05, q=1.1)
-k2 = q_gaussian_kernel(ksize=9, beta=0.05, q=1.1)
 
+k1 = q_gaussian_kernel(ksize=3, sigma=sigma1, q=1.5)
+k2 = q_gaussian_kernel(ksize=11, sigma=sigma2, q=1.5)
+
+print('q-DoG')
+start = time.time()
 blur1 = cv2.filter2D(img_gray, -1, k1)
 blur2 = cv2.filter2D(img_gray, -1, k2)
-
 qdog = blur1.astype('float32') - blur2.astype('float32')
+qdog = np.abs(qdog)   # keep only magnitude
+
+qdog_norm = cv2.normalize(qdog, None, 0, 255, cv2.NORM_MINMAX)
+qdog_norm = qdog_norm.astype(np.uint8)
+print(f'Time spent: {(time.time() - start):.6f} seconds\n')
+
 
 # display all the images
 titles = ['Original', 'Gray', 'Blurred', 'qGauss']
-edge_titles = ['SobelX', 'SobelY', 'SobelXY', 'Canny', 'LoG', 'DoG', 'qDoG']
+edge_titles = ['Sobel', 'Canny', 'LoG', 'DoG', 'qDoG']
 images = [img, img_gray, img_blur, img_qblur]
-edge_images = [sobelx, sobely, sobelxy, edges, log, dog, qdog]
+edge_images = [sobel, canny, log_norm, dog_norm, qdog_norm]
 
 plt.figure(figsize=(10, 10))
 for i in range(len(images)):
@@ -88,14 +125,11 @@ for i in range(len(images)):
         plt.imshow(cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB))
     plt.title(titles[i])
     plt.axis('off')
-    plt.savefig(f'images/{i}_{titles[i]}')
+    plt.savefig(f'images/{i+1}_{titles[i]}')
 
 plt.figure(figsize=(10, 10))
 for i in range(len(edge_images)):
-    if len(edge_images[i].shape) == 2:
-        plt.imshow(edge_images[i], cmap='gray')
-    else:
-        plt.imshow(cv2.cvtColor(edge_images[i], cv2.COLOR_BGR2RGB))
+    plt.imshow(edge_images[i], cmap='gray')
     plt.title(edge_titles[i])
     plt.axis('off')
-    plt.savefig(f'edges/{i}_{edge_titles[i]}')
+    plt.savefig(f'edges/{i+1}_{edge_titles[i]}')
