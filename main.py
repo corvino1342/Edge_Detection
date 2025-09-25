0139d0100df30e002aa16161c3da4dd33ad89824
corvino <corvino1342@gmail.com>
2025-09-25 11:57:04 +0200

    I think the final version. Just one or two steps to get some statistical results

--- main.py
+++ main.py
@@ -76,51 +76,76 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #  blurring the image
 img_blur = cv2.GaussianBlur(img_gray, (3, 3), sigmaX=0)
 
+ksize = 5
+times = []
 
-# Sobel Edge Detection
+#-------------------------------------
 print('Sobel')
+#-------------------------------------
+
 start = time.time()
-gx = cv2.Sobel(img_blur, cv2.CV_64F, 1, 0, ksize=7)
-gy = cv2.Sobel(img_blur, cv2.CV_64F, 0, 1, ksize=7)
+
+gx = cv2.Sobel(img_blur, cv2.CV_64F, 1, 0, ksize=ksize)
+gy = cv2.Sobel(img_blur, cv2.CV_64F, 0, 1, ksize=ksize)
 sobel_mag = np.sqrt(gx**2 + gy**2)
-sobel = normalize_edge_map(sobel_mag)
-print(f'Time spent: {(time.time() - start):.6f} seconds\n')
+sobel_map = normalize_edge_map(sobel_mag)
+t = time.time() - start
+times.append(time.time() - start)
+print(f'Time spent: {t:.6f} seconds\n')
 
-# Canny Edge Detection
+
+#-------------------------------------
 print('Canny')
-start = time.time()
-canny_map = cv2.Canny(image=img_blur, threshold1=80, threshold2=120)
-print(f'Time spent: {(time.time() - start):.6f} seconds\n')
+#-------------------------------------
 
-print('LoG')
 start = time.time()
-lg_k = log_kernel(ksize=7, sigma=1.0)
+
+canny_map = cv2.Canny(image=img_blur, threshold1=80, threshold2=120)
+t = time.time() - start
+times.append(time.time() - start)
+print(f'Time spent: {t:.6f} seconds\n')
+
+#-------------------------------------
+print('LoG')
+#-------------------------------------
+
+start = time.time()
+
+lg_k = log_kernel(ksize=ksize, sigma=1.0)
 log = cv2.filter2D(img_gray, ddepth=cv2.CV_32F, kernel=lg_k)
 log_map = normalize_edge_map(log)
-print(f'Time spent: {(time.time() - start):.6f} seconds\n')
-
+t = time.time() - start
+times.append(time.time() - start)
+print(f'Time spent: {t:.6f} seconds\n')
 sigma1 = 1
 sigma2 = 2
 
+#-------------------------------------
 print('DoG')
+#-------------------------------------
+
 start = time.time()
 
-g_k1 = gaussian_kernel(ksize=7, sigma=1.0)
-g_k2 = gaussian_kernel(ksize=7, sigma=2.0)
+g_k1 = gaussian_kernel(ksize=ksize, sigma=1.0)
+g_k2 = gaussian_kernel(ksize=ksize, sigma=2.0)
 blur1 = cv2.filter2D(img_gray, ddepth=cv2.CV_32F, kernel=g_k1)
 blur2 = cv2.filter2D(img_gray, ddepth=cv2.CV_32F, kernel=g_k2)
 
 dog = blur1.astype('float32') - blur2.astype('float32')
-
 dog_map = normalize_edge_map(dog)
 
-print(f'Time spent: {(time.time() - start):.6f} seconds\n')
+t = time.time() - start
+times.append(time.time() - start)
+print(f'Time spent: {t:.6f} seconds\n')
 
+#-------------------------------------
 print('q-DoG')
+#-------------------------------------
+
 start = time.time()
 
-qg_k1 = q_gaussian_kernel(ksize=7, sigma=1.0, q=0.02)
-qg_k2 = q_gaussian_kernel(ksize=7, sigma=2.0, q=0.02)
+qg_k1 = q_gaussian_kernel(ksize=ksize, sigma=1.0, q=0.02)
+qg_k2 = q_gaussian_kernel(ksize=ksize, sigma=2.0, q=0.02)
 
 qblur1 = cv2.filter2D(img_gray, ddepth=cv2.CV_32F, kernel=qg_k1)
 qblur2 = cv2.filter2D(img_gray, ddepth=cv2.CV_32F, kernel=qg_k2)
@@ -129,9 +154,14 @@ qdog = qblur1.astype('float32') - qblur2.astype('float32')
 
 qdog_map = normalize_edge_map(qdog)
 
-print(f'Time spent: {(time.time() - start):.6f} seconds\n')
+t = time.time() - start
+times.append(time.time() - start)
+print(f'Time spent: {t:.6f} seconds\n')
 
-# display all the images
+
+
+
+
 titles = ['Original', 'Gray', 'Blurred', 'qGauss']
 edge_titles = ['Sobel', 'Canny', 'LoG', 'DoG', 'qDoG']
 edge_images = [sobel_map, canny_map, log_map, dog_map, qdog_map]
@@ -142,3 +172,11 @@ for i in range(len(edge_images)):
     plt.title(edge_titles[i])
     plt.axis('off')
     plt.savefig(f'edges/{i+1}_{edge_titles[i]}')
+
+
+plt.figure(figsize=(12, 6))
+for i in range(len(edge_images)):
+    plt.ylabel("Time [s]")
+    plt.scatter(edge_titles[i], times[i], alpha=0.5, label=edge_titles[i], s=200)
+    plt.grid()
+plt.savefig(f'edges/times.png')
